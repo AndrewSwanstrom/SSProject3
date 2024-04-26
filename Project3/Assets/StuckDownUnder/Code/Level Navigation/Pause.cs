0e70ae66b6482159a210ef9c9ff1222924ddb1b4
BSoto413 <149329491+BSoto413@users.noreply.github.com>
2024-04-26 18:47:14 -0400

    HTP and Pause Final

--- Project3/Assets/StuckDownUnder/Code/Level Navigation/Pause.cs
+++ Project3/Assets/StuckDownUnder/Code/Level Navigation/Pause.cs
@@ -17,7 +17,7 @@ public class Pause : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P) && !isPaused)
+        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused)
         {
             Time.timeScale = 0f;
             pauseScreen.SetActive(true);
